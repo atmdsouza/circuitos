573bf20b8753de3c6473a38fa9fe3a43c7d64c46
André Souza <atmdsouza@gmail.com>
2019-06-24 11:35:05 -0300

    Principais mudanças para a versão 1.04.09: - Hotfix de reunião em 10/06/2019; - Ajuste para produção;

--- app/config/constantes.php
+++ app/config/constantes.php
@@ -7,8 +7,8 @@
  */
 
 //Constantes de Caminho para Arquivos
-define("CAMINHO_PADRAO_RELATORIOS", "/circuitos/public/relatorios/");
-//define("CAMINHO_PADRAO_RELATORIOS", "/public/relatorios/");
+//define("CAMINHO_PADRAO_RELATORIOS", "/circuitos/public/relatorios/");
+define("CAMINHO_PADRAO_RELATORIOS", "/public/relatorios/");
 
 //Versão do sistema
 define("VERSAO", "1.04.09");//Versão em 17/06/2019
