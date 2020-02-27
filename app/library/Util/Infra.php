2b018c6d0af972a4cb85b3432a3eec678be0bca3
André Souza <atmdsouza@gmail.com>
2020-02-27 19:23:42 -0300

    Ajuste das versão do sistema para publicação.

--- app/library/Util/Infra.php
+++ app/library/Util/Infra.php
@@ -37,7 +37,7 @@ class Infra
 
     public function getVersao()
     {
-        return '2.01.03';//Correção dos tipos de lista de valores, nomenclatura do modal de estação telecon
+        return '2.01.04';//Correções solicitadas pelo Igor em 05/02/2020
     }
 
     public function getCaminhoRelatorios()
